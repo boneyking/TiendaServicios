d72e4521e075d89fbf01fb399f453d11f34de9a8
Fabricio Tello Ramirez <fa.tello.ramirez@gmail.com>
2021-10-14 20:41:10 -0300

    Agregar archivos de proyecto.

---
+++ TiendaServicios.Api.Libro/Persistencia/ContextoLibreria.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TiendaServicios.Api.Libro.Modelo;
+
+namespace TiendaServicios.Api.Libro.Persistencia
+{
+    public class ContextoLibreria : DbContext
+    {
+        public ContextoLibreria(DbContextOptions<ContextoLibreria> options) : base(options)
+        {
+
+        }
+
+        public ContextoLibreria()
+        {
+
+        }
+
+        public virtual DbSet<LibreriaMaterial> LibreriaMaterial { get; set; }
+    }
+}
