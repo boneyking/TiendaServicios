2abec59b25bf0fad26be93d982e93f0aed0f0f6d
Fabricio Tello Ramirez <fa.tello.ramirez@gmail.com>
2021-10-14 21:45:48 -0300

    Agrega comentario

--- TiendaServicios.Api.Libro.Tests/LibrosServiceTest.cs
+++ TiendaServicios.Api.Libro.Tests/LibrosServiceTest.cs
@@ -17,6 +17,8 @@ namespace TiendaServicios.Api.Libro.Tests
     {
         private IEnumerable<LibreriaMaterial> ObtenerDataPrueba()
         {
+            //aca se llena info random
+
             A.Configure<LibreriaMaterial>()
                 .Fill(x => x.Titulo).AsArticleTitle()
                 .Fill(x => x.LibreriaMaterialId, () => { return Guid.NewGuid(); });
